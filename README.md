cf85ca4188a11e8eccae5bf438e4b973d959114a
Jabari Hunt <jabari.hunt@performics.com>
2021-02-24 18:16:54 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,116 @@
+# MySQL Handler
+
+A simple class that handles MySQL database instances with support for both TCP and Socket connections.  I created this class as a standard way to interact with MySQL databases from within other projects with very low overhead.  I will personally use it as a dependency in those projects, but it's perfectly capable of being used as a standalone handler.
+
+## Getting Started
+
+### Prerequisites
+
+You will need to have the following environment variables present (usually done with a .env file):
+
+```dotenv
+MYSQL_HOST='localhost'
+MYSQL_NAME='manevia_db'
+MYSQL_USER='root'
+MYSQL_PASSWORD='secretPassword'
+MYSQL_SOCKET=NULL
+```
+
+Depending on how you load environment variables, you may be able to reference already defined variables when setting the required environment variables above.  This helps prevent having to maintain values in multiple locations.  For example:
+
+```dotenv
+DATABASE_HOST='localhost'
+DATABASE_NAME='manevia_db'
+DATABASE_USER='root'
+DATABASE_PASSWORD='secretPassword'
+DATABASE_SOCKET=NULL
+
+MYSQL_HOST='${DATABASE_HOST}'
+MYSQL_NAME='${DATABASE_NAME}'
+MYSQL_USER='${DATABASE_USER}'
+MYSQL_PASSWORD='${DATABASE_PASSWORD}'
+MYSQL_SOCKET='${DATABASE_SOCKET}'
+```
+### Installing
+
+##### Via Composer
+
+Run the following command in the same directory as your composer.json file:
+
+`composer require jabarihunt/mysql`
+
+##### Via Github
+
+1. Clone this repository into a working directory: `git clone git@github.com:jabarihunt/mysql.git`
+
+2. Include or require the MySQL class in your project...
+
+```php
+require('/path/to/cloned/directory/src/MySQL.php');
+```
+...or if using an auto-loader...
+
+```php
+ use jabarihunt/MySQL;
+```
+
+## Usage
+
+This class does not require instantiation since it uses the singleton design pattern for connections.  You may simply begin using the available public methods.  If none of the methods are called, a database connection is never created.  You may alias the class as another name using `use/as`: 
+
+```
+use jabarihunt/MySQL as DB;
+
+/*
+ * QUERY THE DATABASE AS A STANDARD QUERY OR AS A PREPARED STATEMENT
+ */
+
+    $results   = DB::query('SELECT email FROM users WHERE id = 357');      // mysqli_result or false
+    $statement = DB::prepare('SELECT email FROM users WHERE id = 357');    // mysqli_statement or false
+    
+/*
+ * SANITIZE VALUES WHEN NOT USING PREPARED STATEMENTS
+ * THIS METHOD USES PHP filter_var() SANITAZATION BASED ON THE DATA TYPE
+ */
+
+    $string = 'Am I a good string or a naughty string?';
+    
+    $string = DB::sanitize($string, DB::DATA_TYPE_INTEGER);    // All characters removed since sanitizing as an int data type!
+    $string = DB::sanitize($string);                           // String remains since method defaults to DB::DATA_TYPE_INTEGER
+    $string = DB::sanitize(NULL);                              // Null values are converted to an empty string before sanitizing
+    
+    // CLASS DATA TYPES ARE BASED ON MYSQL DATA TYPES -> AVAILABLE DATA TYPES BELOW
+    
+        const DATA_TYPE_INTEGER  = ['tinyint', 'smallint', 'mediumint', 'int', 'bigint', 'bit'];
+        const DATA_TYPE_REAL     = ['float', 'double', 'decimal'];
+        const DATA_TYPE_TEXT     = ['char', 'varchar', 'tinytext', 'text', 'mediumtext', 'longtext'];
+        const DATA_TYPE_BINARY   = ['binary', 'varbinary', 'blob', 'tinyblob', 'mediumblob', 'longblob'];
+        const DATA_TYPE_TEMPORAL = ['date', 'time', 'year', 'datetime', 'timestamp'];
+        const DATA_TYPE_SPATIAL  = ['point', 'linestring', 'polygon', 'geometry', 'multipoint', 'multilinestring', 'multipolygon', 'geometrycollection'];
+        const DATA_TYPE_OTHER    = ['enum', 'set'];
+        
+/*
+ * GET MySQLi OBJECT
+ */
+ 
+    $mysqlObject = DB::getMySQLObject();
+  
+/*
+ * MYSQL DUMP -> CONVENIENCE METHOD FOR DUMPING A DATABASE SOMEWHERE ON THE MACHINE | USE WITH CAUTION!!!
+ * CREATES MYSQL DUMP FILE AT GIVEN PATH WITH FORMAT: <database>_<year>-<month>-<day>_<unix timestamp>.sql
+ */
+ 
+    DB::backup('path/to/my/backup/folder');   // 
+```
+
+## Contributing
+
+1. Fork Repository
+2. Create a descriptive branch name
+3. Make edits to your branch
+4. Squash (rebase) your commits
+5. Create a pull request
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/MySQL.php
@@ -0,0 +1,210 @@
+<?php namespace jabarihunt;
+
+    use ErrorException;
+    use JetBrains\PhpStorm\Pure;
+    use mysqli;
+    use mysqli_result;
+    use mysqli_stmt;
+
+    /********************************************************************************
+     * MYSQL HANDLER
+     * @author Jabari J. Hunt <jabari@jabari.net>
+     *
+     * A simple class that handles MySQL database instances with support for both TCP and Socket connections.  I created this class as a standard way to interact with MySQL databases from within other projects with very low overhead.  I will personally use it as a dependency in those projects, but it's perfectly capable of being used as a standalone handler.
+     *
+     ********************************************************************************/
+
+        class MySQL {
+
+            /********************************************************************************
+             * CONNECTION VARIABLES
+             * @var string $host Database Server
+             * @var string $database Database Instance
+             * @var string $user Database Username
+             * @var string $password Database Password
+             * @var string $socket Database Socket
+             ********************************************************************************/
+
+                private static string|null $host;
+                private static string $database;
+                private static string $user;
+                private static string $password;
+                private static string|null $socket;
+
+            /********************************************************************************
+             * CLASS VARIABLES
+             * @var object $db MySQLi instance
+             * @var object $instance Singleton instance of this class
+             ********************************************************************************/
+
+                private static mysqli $db;
+                private static MySQL|null $instance = NULL;
+
+            /********************************************************************************
+             * CLASS CONSTANTS
+             * @var integer DATA_TYPE_INTEGER  - tinyint, smallint, mediumint, int, bigint, bit
+             * @var integer DATA_TYPE_REAL     - float, double, decimal
+             * @var integer DATA_TYPE_TEXT     - char, varchar, tinytext, text, mediumtext, longtext
+             * @var integer DATA_TYPE_BINARY   - binary, varbinary, blob, tinyblob, mediumblob, longblob
+             * @var integer DATA_TYPE_TEMPORAL - date, time, year, datetime, timestamp
+             * @var integer DATA_TYPE_SPATIAL  - point, linestring, polygon, geometry, multipoint, multilinestring, multipolygon, geometrycollection
+             * @var integer DATA_TYPE_OTHER    - enum, set
+             ********************************************************************************/
+
+                const DATA_TYPE_INTEGER  = ['tinyint', 'smallint', 'mediumint', 'int', 'bigint', 'bit'];
+                const DATA_TYPE_REAL     = ['float', 'double', 'decimal'];
+                const DATA_TYPE_TEXT     = ['char', 'varchar', 'tinytext', 'text', 'mediumtext', 'longtext'];
+                const DATA_TYPE_BINARY   = ['binary', 'varbinary', 'blob', 'tinyblob', 'mediumblob', 'longblob'];
+                const DATA_TYPE_TEMPORAL = ['date', 'time', 'year', 'datetime', 'timestamp'];
+                const DATA_TYPE_SPATIAL  = ['point', 'linestring', 'polygon', 'geometry', 'multipoint', 'multilinestring', 'multipolygon', 'geometrycollection'];
+                const DATA_TYPE_OTHER    = ['enum', 'set'];
+
+            /********************************************************************************
+             * SINGLETON INSTANCE METHOD
+             * @return mysqli|null
+             ********************************************************************************/
+
+                private static function get(): mysqli|null {
+
+                    if (self::$instance === NULL) {self::$instance = new MySQL();}
+                    return self::$db;
+
+                }
+
+            /********************************************************************************
+             * CONSTRUCTOR
+             * @throws ErrorException
+             ********************************************************************************/
+
+                private function __construct() {
+
+                    // SET DATABASE AND SESSION CLASS VARIABLES
+
+                        self::$host           = getenv('MYSQL_HOST');
+                        self::$database       = getenv('MYSQL_NAME');
+                        self::$user           = getenv('MYSQL_USER');
+                        self::$password       = getenv('MYSQL_PASSWORD');
+
+                        if (
+                            !empty(getenv('DATABASE_SOCKET')) &&
+                            getenv('DATABASE_SOCKET') !== 'NULL'
+                        ) {
+                            self::$socket = getenv('DATABASE_SOCKET');
+                        } else {
+                            self::$socket = NULL;
+                        }
+
+                    // CONNECT TO DATABASE | CHECK CONNECTION
+
+                        self::$db = (self::$socket === NULL) ? new mysqli(self::$host, self::$user, self::$password, self::$database) : new mysqli(NULL, self::$user, self::$password, self::$database, NULL, self::$socket);
+
+                        if (self::$db->connect_errno > 0) {
+
+                            $connectionError = 'MySQL Connection Error #' . self::$db->connect_errno . ': ' . self::$db->connect_error;
+                            throw new ErrorException($connectionError, 0, E_ERROR, __FILE__, __LINE__);
+
+                        }
+
+                }
+
+            /********************************************************************************
+             * DESTRUCTOR
+             ********************************************************************************/
+
+                public function __destruct() {
+
+                    // CLOSE DATABASE
+
+                        if (self::$db instanceof mysqli) {
+                            self::$db->close();
+                        }
+
+                }
+
+            /********************************************************************************
+             * PUBLIC DB METHODS -> BACKUP | SET INSTANCE | PREPARE | QUERY
+             ********************************************************************************/
+
+                /********************************************************************************
+                 * QUERY METHOD
+                 * @param string $query
+                 * @return mysqli_result|false
+                 ********************************************************************************/
+
+                    public static function query(string $query): mysqli_result|false {
+                        return self::get()->query($query);
+                    }
+
+                /********************************************************************************
+                 * PREPARE METHOD
+                 * @param string $query
+                 * @return mysqli_stmt|false
+                 ********************************************************************************/
+
+                    public static function prepare(string $query): mysqli_stmt|false {
+                        return self::get()->prepare($query);
+                    }
+
+                /********************************************************************************
+                 * SANITIZE METHOD
+                 * Used to sanitize individual field values for database insertion.
+                 * @param mixed $value The value to be sanitized.
+                 * @param string|null $dataType The DB datatype of the passed value
+                 * @return string|int|float|null
+                 *******************************************************************************/
+
+                    #[Pure]
+                    public static function sanitize(mixed $value, array $dataType = self::DATA_TYPE_TEXT): string|int|float {
+
+                        // MAKE SURE VALUE ISN'T NULL | SANITIZE BASED ON DATA TYPE | RETURN VALUE
+
+                            $value = ($value === NULL) ? '' : $value;
+
+                            switch ($dataType) {
+                                case self::DATA_TYPE_TEXT:
+                                    $value = filter_var($value, FILTER_SANITIZE_STRING);
+                                    break;
+                                case self::DATA_TYPE_INTEGER:
+                                    $value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+                                    break;
+                                case self::DATA_TYPE_REAL:
+                                    $value = filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                                    break;
+                                default:
+                                    $value = filter_var($value, FILTER_SANITIZE_STRING);
+                            }
+
+                            return $value;
+
+                    }
+
+                /********************************************************************************
+                 * GET MYSQL OBJECT METHOD
+                 * @return mysqli
+                 ********************************************************************************/
+
+                    public static function getMySQLObject(): mysqli {
+                        return self::get();
+                    }
+
+                /********************************************************************************
+                 * BACKUP METHOD
+                 * @param string $directory
+                 * @return void
+                 ********************************************************************************/
+
+                    public static function backup(string $directory): void {
+
+                $user     = self::$user;
+                $password = self::$password;
+                $database = self::$database;
+                $location = rtrim($directory, '/') . "/{$database}-" . date('Y-m-d') . '_' . time() . '.sql';
+
+                exec("mysqldump --user='{$user}' --password='{$password}' --single-transaction --routines --triggers {$database} > {$location}");
+
+            }
+
+
+        }
+
+?>
